9e6ff7a6954119e217e75d8aa3c88d84a82a3977
jburhenn <jburhenn@mydevices.com>
2016-06-07 12:24:30 -0600

    The GenericAnalgoOutput should actually be a GenericPWMOutput sketch.

--- Actuators/GenericPWMOutput/GenericPWMOutput.ino
+++ Actuators/GenericPWMOutput/GenericPWMOutput.ino
@@ -1,16 +1,16 @@
 /*
-Cayenne Generic Analog Output Example
+Cayenne Generic PWM Output Example
 
-This sketch shows how to set up a Generic Analog Output with Cayenne.
+This sketch shows how to set up a Generic PWM Output with Cayenne.
 
 Steps:
-1. In the Cayenne Dashboard add a new Generic Analog Output.
-2. Select an ADC (Analog) pin number.
-3. Attach an analog ouput device to the analog pin on your Arduino matching the selected pin.
+1. In the Cayenne Dashboard add a new Generic PWM Output.
+2. Select a digital PWM pin number (3, 5, 6, 9, 10, and 11 on most Arduino boards).
+3. Attach an analog ouput device to the PWM pin on your Arduino matching the selected pin.
 4. Set the token variable to match the Arduino token from the Dashboard.
 5. Compile and upload this sketch.
 
-Notice that there isn't much coding involved to interact with the analog pins.
+Notice that there isn't much coding involved to interact with the PWM pins.
 Most of it is handled automatically from the Cayenne library.
 */
 
