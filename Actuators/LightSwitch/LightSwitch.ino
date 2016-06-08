384b5438c896b29bae1176ed67ba5ee9d389dfce
jburhenn <jburhenn@mydevices.com>
2016-06-08 10:05:34 -0600

    3073 Fixed typo.

--- Actuators/LightSwitch/LightSwitch.ino
+++ Actuators/LightSwitch/LightSwitch.ino
@@ -6,7 +6,7 @@ This sketch shows how to set up a Light Switch with Cayenne
 Steps:
 1. In the Cayenne Dashboard add a new Light Switch Widget.
 2. Select a GPIO pin number.
-3. Attach the positive leg of an LED to ground and the other leg to the selected digital pin.
+3. Attach the negative leg of an LED to ground and the other leg to the selected digital pin.
    Schematic:
    [Ground] -- [LED] -- [Resistor] -- [Digital Pin]
 4. Set the token variable to match the Arduino token from the Dashboard.
