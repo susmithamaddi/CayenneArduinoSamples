14265417d6a8c741cf0f0a965d58144e32563bfe
jburhenn <jburhenn@mydevices.com>
2016-06-14 11:20:44 -0600

    Added Ethernet Shield 2 example.

---
+++ Connections/ArduinoEthernet2/ArduinoEthernet2.ino
@@ -0,0 +1,30 @@
+/*
+Cayenne Ethernet Shield 2 Example
+Adapted from Blynk's Arduino_Ethernet Example
+
+This sketch connects to the Cayenne server using an Arduino Ethernet Shield 2
+and runs the main communication loop.
+
+For Cayenne Dashboard widgets using digital or analog pins this sketch will automatically
+send data on those pins to the Cayenne server. If the widgets use Virtual Pins, data
+should be sent to those pins using virtualWrites. Examples for sending and receiving
+Virtual Pin data are under the Basics folder.
+*/
+
+//#define CAYENNE_DEBUG         // Uncomment to show debug messages
+#define CAYENNE_PRINT Serial  // Comment this out to disable prints and save space
+#include <CayenneEthernetW5500.h>
+
+// Cayenne authentication token. This should be obtained from the Cayenne Dashboard.
+char token[] = "AuthenticationToken";
+
+void setup()
+{
+	Serial.begin(9600);
+	Cayenne.begin(token);
+}
+
+void loop()
+{
+	Cayenne.run();
+}
