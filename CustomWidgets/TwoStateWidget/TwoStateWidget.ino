123526b419fe35fbc4deb9eab362efad8a337abd
jburhenn <jburhenn@mydevices.com>
2016-12-06 16:19:28 -0700

    Updates for additional two state and value widgets.

--- CustomWidgets/TwoStateWidget/TwoStateWidget.ino
+++ CustomWidgets/TwoStateWidget/TwoStateWidget.ino
@@ -1,17 +1,22 @@
 /*
-Cayenne 2 State Widget Example
+Cayenne Two State Display Example
 
-This sketch shows how to set up a 2 State Widget with Cayenne.
+This sketch shows how to set up a two state display widget with Cayenne. This sketch can be used with any widget that displays two states including:
+* 2 State
+* Open/Close
+* CO2 Detector
+* Motion Detector
+* any other two state display widgets
 
 The Cayenne Library is required to run this sketch. If you have not already done so you can install it from the Arduino IDE Library Manager.
 
 Steps:
-1. In the Cayenne Dashboard add a new 2 State Custom Widget.
+1. In the Cayenne Dashboard add a new two state custom widget, e.g 2 State, Motion Detector, etc.
 2. Select a digital pin number. Do not use digital pins 0 or 1 since those conflict with the use of Serial.
 3. Attach a digital input device (e.g. a button) to the digital pin on your Arduino matching the selected pin.
 4. Set the token variable to match the Arduino token from the Dashboard.
 5. Compile and upload this sketch.
-6. Once the Arduino connects to the Dashboard it should automatically update the 2 State widget with data.
+6. Once the Arduino connects to the Dashboard it should automatically update the widget with data.
 
 Notice that there isn't much coding involved to interact with the digital pins.
 Most of it is handled automatically from the Cayenne library.
--- CustomWidgets/ValueWidget/ValueWidget.ino
+++ CustomWidgets/ValueWidget/ValueWidget.ino
@@ -1,12 +1,18 @@
 /*
-Cayenne Value Widget Example
+Cayenne Value Display Example
 
-This sketch shows how to set up a Value Widget with Cayenne.
+This sketch shows how to set up a value display widget with Cayenne. This sketch can be used with any widget that displays a value including:
+* Value
+* Line Chart
+* Gauge
+* Meter
+* Emoji/Text Status
+* any other value display widgets
 
 The Cayenne Library is required to run this sketch. If you have not already done so you can install it from the Arduino IDE Library Manager.
 
 Steps:
-1. In the Cayenne Dashboard add a new Value Custom Widget.
+1. In the Cayenne Dashboard add a new value display widget, e.g. Value, Meter, Gauge, etc.
 3. Select Virtual I/O and a virtual pin number.
 4. Set VIRTUAL_PIN to the pin number you selected.
 5. Select a data type and unit, e.g. Temperature, Celsius.
@@ -14,7 +20,7 @@ Steps:
 7. Set the token variable to match the Arduino token from the Dashboard.
 8. Modify the CAYENNE_OUT(VIRTUAL_PIN) function to send data from your sensor.
 9. Compile and upload this sketch.
-10. Once the Arduino connects to the Dashboard it should automatically update the Value widget with data.
+10. Once the Arduino connects to the Dashboard it should automatically update the widget with data.
 
 For further examples of how to send data to Cayenne see the example sketches under Sensors.
 */
